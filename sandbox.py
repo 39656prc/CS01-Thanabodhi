70a8160cbb18d9a1309e956b19d5909d694aaabe
39656prc <136313806+39656prc@users.noreply.github.com>
2023-07-10 16:05:59 +0800

    Create sandbox.py

---
+++ sandbox.py
@@ -0,0 +1,7 @@
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+z = int(input("Enter z: "))
+if (x == y == z):
+    print("x, y and z are equal")
+else:
+    print("x, y and z are not equal")
