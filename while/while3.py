7d06d342fe53e3ab2f1606499bcb8ff66fbc66a2
39656prc <136313806+39656prc@users.noreply.github.com>
2023-08-21 16:56:00 +0800

    while

---
+++ while/while3.py
@@ -0,0 +1,9 @@
+n = 0
+x = -1
+while True:
+    x = int(input())
+    if (x == 0):
+        break
+    n += 1
+    print(n)
+print(n)
---
+++ while/while5.py
@@ -0,0 +1,5 @@
+n = int(input())
+while n <= 1000:
+    print(n)
+    n *= 2
+print(n)
