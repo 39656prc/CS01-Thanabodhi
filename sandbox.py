64ebf03be4c4bd4cc4ece0cb3f349b5d737dbaa6
39656prc <136313806+39656prc@users.noreply.github.com>
2023-10-26 21:39:33 +0800

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,23 @@
-x = int(input("Enter x: "))
-y = int(input("Enter y: "))
-z = int(input("Enter z: "))
-if (x == y == z):
-    print("x, y and z are equal")
+import math
+from math import floor
+a = str(input())
+if (a == "W"):
+    print("ติด ร")
 else:
-    print("x, y and z are not equal")
+    b = str(input())
+    c = str(input())
+    d = str(input())
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+    sum = a+b+c+d
+    if (sum >= 80):
+        grade = 4
+    elif (sum >= 50):
+        grade = (floor((sum-40)/5))/2
+    else:
+        grade = 0
+    if (not(grade%1)):
+        grade = int(grade)
+    print(grade)
